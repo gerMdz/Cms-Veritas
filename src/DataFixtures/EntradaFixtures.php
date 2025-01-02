1c287a046bcdc9e7dae15450d55009680261fdd9
germdz <gerardo.montivero@gmail.com>
2025-01-02 11:02:55 -0300

    Agregando factories correctamente.

--- src/DataFixtures/EntradaFixtures.php
+++ src/DataFixtures/EntradaFixtures.php
@@ -3,8 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Entrada;
-// use App\Entity\Comment;
-// use App\Entity\Tag;
+use App\Entity\User;
 use App\Service\UploaderHelper;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -13,47 +12,36 @@ use Symfony\Component\HttpFoundation\File\File;
 
 class EntradaFixtures extends BaseFixture implements DependentFixtureInterface
 {
-    private static $entradaTitles = [
+    private array $entradaTitles = [
         'Ante lo Inesperado',
         'Indomita',
         'Rescate en las llamas',
         'Maravillas cotidianas',
     ];
 
-    private static $entradaImages = [
+    private array $entradaImages = [
         '01-Ante-lo-inesperado.jpg',
         '02-Indomita.jpg',
         '03-Rescate-en-las-llamas.jpg',
     ];
 
-    /**
-     * EntradaFixtures constructor.
-     */
-    public function __construct(private readonly UploaderHelper $uploaderHelper)
+    public function __construct(private UploaderHelper $uploaderHelper)
     {
     }
 
-    protected function loadData(ObjectManager $manager)
+    protected function loadData(ObjectManager $manager): void
     {
-        $this->createMany(10, 'main_entradas', function ($count) {
+        $this->createMany(10, 'main_entradas', function () {
             $entrada = new Entrada();
-            $entrada->setTitulo($this->faker->randomElement(self::$entradaTitles))
-                ->setContenido('Una dato más');
+            $entrada->setTitulo($this->faker->randomElement($this->entradaTitles))
+                ->setContenido('Una dato más')
+                ->setLinkRoute(strtolower(str_replace(' ', '-', trim($entrada->getTitulo() . random_int(0, 100)))))
+                ->setImageFilename($this->fakeUploadImage())
+                ->setAutor($this->getRandomReference('escitor_users', User::class));
 
-            // publish most articles
             if ($this->faker->boolean(70)) {
                 $entrada->setPublicadoAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
-            $imageFilename = $this->fakeUploadImage();
-            $link = strtolower(str_replace(' ', '-', trim($entrada->getTitulo().' '.random_int(0, 100))));
-            $entrada->setAutor($this->getRandomReference('escitor_users'))
-                ->setImageFilename($imageFilename)
-                ->setLinkRoute($link);
-
-            //            $tags = $this->getRandomReferences('main_tags', $this->faker->numberBetween(0, 5));
-            //            foreach ($tags as $tag) {
-            //                $article->addTag($tag);
-            //            }
 
             return $entrada;
         });
@@ -63,19 +51,17 @@ class EntradaFixtures extends BaseFixture implements DependentFixtureInterface
 
     private function fakeUploadImage(): string
     {
-        $randomImage = $this->faker->randomElement(self::$entradaImages);
-        $fs = new Filesystem();
-        $targetPath = sys_get_temp_dir().'/'.$randomImage;
-        $fs->copy(__DIR__.'/images/'.$randomImage, $targetPath, true);
+        $randomImage = $this->faker->randomElement($this->entradaImages);
+        $filesystem = new Filesystem();
+        $targetPath = sys_get_temp_dir() . '/' . $randomImage;
+        $filesystem->copy(__DIR__ . '/images/' . $randomImage, $targetPath, true);
 
-        return $this->uploaderHelper
-            ->uploadEntradaImage(new File($targetPath), false);
+        return $this->uploaderHelper->uploadEntradaImage(new File($targetPath), false);
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
-//            TagFixture::class,
             UserFixtures::class,
         ];
     }
--- src/DataFixtures/PrincipalFixtures.php
+++ src/DataFixtures/PrincipalFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Entrada;
 use App\Entity\Principal;
+use App\Entity\User;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
@@ -17,7 +19,7 @@ class PrincipalFixtures extends BaseFixture implements DependentFixtureInterface
         'avanza',
     ];
 
-    protected function loadData(ObjectManager $manager)
+    protected function loadData(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
@@ -29,15 +31,15 @@ class PrincipalFixtures extends BaseFixture implements DependentFixtureInterface
                 ->setCreatedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
 
             $principal->setLinkRoute(self::$principalTitles[$indice]);
-            $principal->setAutor($this->getRandomReference('escitor_users'));
-            $principal->addEntrada($this->getRandomReference('main_entradas'));
+            $principal->setAutor($this->getRandomReference('escitor_users', User::class));
+            $principal->addEntrada($this->getRandomReference('main_entradas', Entrada::class));
 
             return $principal;
         });
         $manager->flush();
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
 //            TagFixture::class,
