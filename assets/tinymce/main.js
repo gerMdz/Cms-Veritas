08376036ff76944a0796691c3572fd6181714414
germdz <gerardo.montivero@gmail.com>
2025-01-20 18:07:21 -0300

    Primer upgrade 5.0.1

--- assets/tinymce/main.js
+++ assets/tinymce/main.js
@@ -1,4 +1,22 @@
 import tinymce from 'tinymce';
+import 'tinymce/themes/silver';
+import 'tinymce/icons/default';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/quickbars';
+import 'tinymce/plugins/autosave';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/visualchars';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/anchor';
+import 'tinymce/plugins/autolink';
+require('./content.min.css');
 
 tinymce.init({
     selector: '.tinymce-editor',
